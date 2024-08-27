b4299190a2c38cc39e60b067c7e8370bdb049a65
Aman Upadhyay <amanupadhyay33822@gmail.com>
2024-08-27 16:41:15 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,9 @@ require('dotenv').config({})
 
 const app = express()
 
-
+app.get('/', (req,res)=>{
+   res.send("hello world")
+})
 app.listen(process.env.PORT, async() => {
     await connectDb();
     console.log("Users api listing on PORT : ",process.env.PORT);
